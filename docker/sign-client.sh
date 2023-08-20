98330c7e310a1d27b3753c1d493c834111123715
Stephen <hyperfact@gmail.com>
2023-08-20 21:25:18 +0800

    shell

--- docker/sign-client.sh
+++ docker/sign-client.sh
@@ -1,24 +1,27 @@
 #!/usr/bin/env sh
-# usage: ./sign-client.sh
+# usage: ./sign-client.sh NAME DAYS
+
+arg_name=${1:-client}
+arg_days=${2:-3650}
 
 # create key
-openssl genrsa -out client.key.pem 4096
+openssl genrsa -out ${arg_name}.key.pem 4096
 
 # create csr
-openssl req -subj '/CN=client' -new -key client.key.pem -out client.csr
+openssl req -subj "/CN=${arg_name}" -new -key ${arg_name}.key.pem -out ${arg_name}.csr
 
 # create extfile
 echo extendedKeyUsage = clientAuth > client-extfile.cnf
 
 # sign
-openssl x509 -req -days 3650 -sha256 -in client.csr -CA /ca/ca.pem -CAkey /ca/ca.key.pem \
-    -CAcreateserial -out client.cert.pem -extfile client-extfile.cnf
+openssl x509 -req -days ${arg_days} -sha256 -in ${arg_name}.csr -CA /ca/ca.pem -CAkey /ca/ca.key.pem \
+    -CAcreateserial -out ${arg_name}.cert.pem -extfile client-extfile.cnf
 
 # access
-chmod -v 0400 client.key.pem
-chmod -v 0444 client.cert.pem
+chmod -v 0400 ${arg_name}.key.pem
+chmod -v 0444 ${arg_name}.cert.pem
 
 # clean
-rm -f client-extfile.cnf client.csr
+rm -f client-extfile.cnf ${arg_name}.csr
 
 cp -f /ca/ca.pem ./
--- docker/sign-server.sh
+++ docker/sign-server.sh
@@ -1,23 +1,26 @@
 #!/usr/bin/env sh
 # usage: HOST=example.com IP=127.0.0.1 ./sign-server.sh
 
+arg_name=${1:-client}
+arg_days=${2:-3650}
+
 # create key
-openssl genrsa -out server.key.pem 4096
+openssl genrsa -out ${arg_name}.key.pem 4096
 
 # create csr
-openssl req -subj "/CN=${HOST}" -sha256 -new -key server.key.pem -out server.csr
+openssl req -subj "/CN=${HOST}" -sha256 -new -key ${arg_name}.key.pem -out ${arg_name}.csr
 
 # create extfile
 echo subjectAltName = DNS:${HOST},IP:${IP} > server-extfile.cnf
 echo extendedKeyUsage = serverAuth >> server-extfile.cnf
 
 # sign
-openssl x509 -req -days 3650 -sha256 -in server.csr -CA /ca/ca.pem -CAkey /ca/ca.key.pem \
-    -CAcreateserial -out server.cert.pem -extfile server-extfile.cnf
+openssl x509 -req -days 3650 -sha256 -in ${arg_name}.csr -CA /ca/ca.pem -CAkey /ca/ca.key.pem \
+    -CAcreateserial -out ${arg_name}.cert.pem -extfile server-extfile.cnf
 
 # access
-chmod -v 0400 server.key.pem
-chmod -v 0444 server.cert.pem
+chmod -v 0400 ${arg_name}.key.pem
+chmod -v 0444 ${arg_name}.cert.pem
 
 # clean
 rm -f server-extfile.cnf
