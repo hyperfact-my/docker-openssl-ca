a54f3fd61e6018727b1dd2cfb8cf425582531177
hyperfact-my <hyperfact.my@gmail.com>
2026-01-07 15:13:27 +0800

    Update sign-client.sh

--- docker/sign-client.sh
+++ docker/sign-client.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env sh
-# usage: ./sign-client.sh NAME DAYS
+# usage: ./sign-client.sh int-ca NAME DAYS
 
-arg_name=${1:-client}
-arg_days=${2:-3650}
+arg_ca=${1:-int-ca}
+arg_name=${2:-client}
+arg_days=${3:-3650}
 
 # create key
-openssl genrsa -out ${arg_name}.key.pem 4096
+# openssl genrsa -out ${arg_name}.key.pem 4096
+openssl ecparam -name prime256v1 -genkey -noout -out ${arg_name}.key.pem
 
 # create csr
-openssl req -subj "/CN=${arg_name}" -new -key ${arg_name}.key.pem -out ${arg_name}.csr
+openssl req -subj "/CN=${arg_name}" -sha256 -new -key ${arg_name}.key.pem -out ${arg_name}.csr
 
 # create extfile
 echo extendedKeyUsage = clientAuth > client-extfile.cnf
 
 # sign
-openssl x509 -req -days ${arg_days} -sha256 -in ${arg_name}.csr -CA /ca/ca.pem -CAkey /ca/ca.key.pem \
+openssl x509 -req -days ${arg_days} -sha256 -in ${arg_name}.csr -CA ${arg_ca}.cert.pem -CAkey ${arg_ca}.key.pem \
     -CAcreateserial -out ${arg_name}.cert.pem -extfile client-extfile.cnf
+
+cat ${arg_name}.cert.pem ${arg_ca}.cert.pem > ${arg_name}-fullchain.crt
 
 # access
 chmod -v 0400 ${arg_name}.key.pem
